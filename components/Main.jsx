58d2312a6736e8deaaaa95c6dd5a4a52de3e5c72
Nitesh Negi <nitesh_negi@outlook.com>
2024-12-20 11:36:10 +0530

    first commit

---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+      <main>
+          <h1>Fun facts about Apple Inc.</h1>
+          <ul className="facts-list">
+              <li>Apple was founded on April 1, 1976, in Cupertino, California.</li>
+              <li>The first Apple logo featured Isaac Newton under an apple tree.</li>
+              <li>Apple's iPhone contributes over 50% of its total revenue globally.</li>
+              <li>Steve Jobs's iconic black turtlenecks were designed by Issey Miyake.</li>
+              <li>Apple's market capitalization surpassed $3 trillion in 2023, a record.</li> 
+          </ul>
+      </main>
+  )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+      <header>
+          <nav>
+              <img src="../R.png" alt="React logo" />
+              <span>AppleFacts</span>
+          </nav>
+      </header>
+  )
+}
