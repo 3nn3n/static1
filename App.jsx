a2c8a67d8d579cb3fe75499dd2c7b99b373650db
Nitesh Negi <nitesh_negi@outlook.com>
2024-12-20 12:14:11 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import Main from "./components/Main"
 import Navbar from "./components/Navbar"
 
+import { HashRouter as Router } from "react-router-dom";
+
 export default function App() {
     return (
+        <Router>
         <>
             <Navbar />
             <Main />
         </>
+        </Router>
     )
 }
